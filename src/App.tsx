d9e02e245c901f2590514e55b45d8c30fa36dbe2
thanhngan22 <nganbui9a2017@gmail.com>
2023-06-12 17:29:10 +0700

    initialize project/ add packages and config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="bg-slate-500 text-fuchsia-300" >hi</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Hoặc 'media' hoặc 'class'
+  theme: {
+    extend: {
+      // Tùy chỉnh theme tại đây
+    },
+  },
+  variants: {
+    extend: {
+      // Tùy chỉnh variants tại đây
+    },
+  },
+  plugins: [
+    // Thêm các plugins tại đây
+  ],
+}
